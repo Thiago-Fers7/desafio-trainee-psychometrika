7d2fd5f8fd9f9dbe73905f745448722b46df9792
Luiz Thiago <thiago_fers7@hotmail.com>
2021-07-29 18:22:34 -0300

    routes and your pages

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,10 +3,20 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 // Pages
 import { NoMatch } from './pages/noMatch'
 import { Login } from './pages/login'
+import { AdminDashboard } from './pages/adminDashboard'
+import { StudentDashboard } from './pages/studentDashboard'
 
 function Routes() {
     return (
         <Switch>
+            <Route path="/dashboard/student">
+                <StudentDashboard />
+            </Route>
+
+            <Route path="/dashboard/admin">
+                <AdminDashboard />
+            </Route>
+
             <Route path="/login">
                 <Login />
             </Route>

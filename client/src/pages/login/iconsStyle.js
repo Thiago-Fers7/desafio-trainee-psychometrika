48ca4d24080af281774cf2f72cb695a0374c0fd4
Luiz Thiago <thiago_fers7@hotmail.com>
2021-07-28 22:03:50 -0300

    structre html from login page

---
+++ client/src/pages/login/iconsStyle.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        color: '#5c939d',
+        position: 'absolute',
+        bottom: '2.4rem',
+        right: '1rem',
+        fontSize: '1.6rem',
+        transform: 'translateY(50%)',
+        cursor: 'pointer'
+    },
+})
+
+export { useStyles }
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,7 +1,81 @@
+import React from 'react'
+import { useState } from 'react'
+import styles from './styles.module.scss'
+
+// Icons
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import { useStyles } from './iconsStyle'
 
 function Login() {
+    const viewStyles = useStyles()
+
+    const [viewPass, setViewPass] = useState<boolean>(false)
+
+    function toggleViewPass() {
+        setViewPass(!viewPass)
+    }
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault()
+    }
+
+    const backgroundStyle = {
+        background: `url("/images/login-background.svg") no-repeat center`,
+        backgroundSize: `cover`
+    }
+
     return (
-        <h1>Hello Psycho</h1>
+        <div className={styles.container} style={backgroundStyle}>
+            <section>
+                <h2 className="sr-only">
+                    Psychometrika
+                </h2>
+
+                <img src="/images/logo.svg" alt="Logo Psychometrika" />
+
+                <p className={styles.descript}>Desafio Trainee</p>
+
+                <form onSubmit={handleSubmit} >
+                    <fieldset>
+                        <legend className="sr-only">
+                            Informe os dados de login
+                        </legend>
+
+                        <div className={styles.inputFields}>
+                            <label>
+                                <span>Email</span>
+                                <input
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Seu email institucional"
+                                />
+                            </label>
+                            <label>
+                                <span>Senha</span>
+                                <input
+                                    type={viewPass ? "text" : "password"}
+                                    id="password"
+                                    name="password"
+                                    placeholder="Mínimo de 8 caracteres"
+                                />
+                                {viewPass ? (
+                                    <VisibilityIcon onClick={toggleViewPass} className={viewStyles.root} />
+                                ) : (
+                                    <VisibilityOffIcon onClick={toggleViewPass} className={viewStyles.root} />
+                                )}
+                            </label>
+                        </div>
+                        <button
+                            type="button"
+                        >
+                            Entrar
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+        </div>
     )
 }
 
